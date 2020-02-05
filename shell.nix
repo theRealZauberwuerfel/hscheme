b3a69f2e5164bc5f3fbd35a8a894591dd32c2f5c
Rubik <rub1k@ctdo.de>
2020-02-05 21:28:46 +0100

    added shell.nix

---
+++ shell.nix
@@ -0,0 +1,33 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, haskeline, integer-gmp, mtl, parsec
+      , stdenv, transformers
+      }:
+      mkDerivation {
+        pname = "hscheme";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base haskeline integer-gmp mtl parsec transformers
+        ];
+        description = "Small Scheme interpreter in Haskell";
+        license = stdenv.lib.licenses.gpl3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
